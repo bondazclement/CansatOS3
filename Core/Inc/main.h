8663eee041e2f1788a2c4f0f665c5cea19071cb2
Clément Bondaz <163433698+bondazclement@users.noreply.github.com>
2025-07-03 23:32:12 +0200

    encore la correction des bugs

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -6,24 +6,19 @@ extern "C" {
 #endif
 
 #include "stm32l4xx_hal.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "cmsis_os.h"
 
-// Définitions des broches
+// Inclure les en-têtes des périphériques
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+#include "freertos.h"
+
+
+// Définitions des broches pour la LED
 #define LD3_Pin GPIO_PIN_3
 #define LD3_GPIO_Port GPIOB
-#define VCP_TX_Pin GPIO_PIN_2
-#define VCP_TX_GPIO_Port GPIOA
-#define VCP_RX_Pin GPIO_PIN_15
-#define VCP_RX_GPIO_Port GPIOA
-
-extern TIM_HandleTypeDef htim1;
-extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart2;
 
 void Error_Handler(void);
-void MX_FREERTOS_Init(void);
 
 
 #ifdef __cplusplus
